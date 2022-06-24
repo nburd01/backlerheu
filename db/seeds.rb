204a7036f8c7e2a6c1630a336611d87899e9de34
Niall Burdon <n.burdon@protonmail.com>
2022-06-24 15:29:37 +0200

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+10.times do |r|
+  user = User.create!(
+    email: "a#{r}@yopmail.com",
+    password: 'aaaaaa'
+    );
+
+end
