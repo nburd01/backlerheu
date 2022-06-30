c3cea83b208b1b15d17f595e5f3c038c680ccf08
Niall Burdon <n.burdon@protonmail.com>
2022-06-30 18:43:01 +0200

    testing db

--- db/migrate/20220629195507_create_sends.rb
+++ db/migrate/20220629195507_create_sends.rb
@@ -1,7 +1,8 @@
 class CreateSends < ActiveRecord::Migration[7.0]
   def change
     create_table :sends do |t|
-
+      t.string :content
+      t.integer :receiver
       t.timestamps
     end
   end
